a63b18703825c387644eb860e6d007e9eb876568
Liam <124311006+18090@users.noreply.github.com>
2023-03-13 01:46:21 +0000

    Added price appends to json

--- main.py
+++ main.py
@@ -8,3 +8,12 @@ if __name__ == '__main__':
 
     if price_data is not None:
         json_price_data = json.loads(price_data)
+
+for d in json_price_data:
+            price.append({'name': d['name'], 'price': float(d['newworld_price']), 'url': d['newworld_url']})
+            price.append({'name': d['name'], 'price': float(d['paknsave_price']), 'url': d['paknsave_url']})
+            price.append({'name': d['name'], 'price': float(d['countdown_price']), 'url': d['countdown_url']})
+            minPricedItem = min(price, key=lambda x: x['price'])
+            print(minPricedItem)
+            print('=================')
+            price = []
