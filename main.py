a081502826744e32cfb04a0dfec1107db13c7649
Liam <124311006+18090@users.noreply.github.com>
2023-03-14 20:37:24 +0000

    Created window for Price Comparison Tool

---
+++ main.py
@@ -0,0 +1,13 @@
+#Price Comparison Tool
+# ____________   IMPORTS ________________
+# used to create a custom window for age calculator
+import tkinter as tk
+from tkinter import ttk
+# ____________   MAIN  ________________
+# Creating a custom window
+window = tk.Tk()
+window.geometry("500x230")
+window.config(bg="#F7DC6F")
+window.resizable(width=False, height=False)
+window.title('Compare Prices Now!')
+
