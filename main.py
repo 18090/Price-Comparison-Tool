a136bacd2fb3500882368c65a586ea7d4787d926
Liam <124311006+18090@users.noreply.github.com>
2023-03-14 20:58:39 +0000

    Improved window structure and added first labels

--- main.py
+++ main.py
@@ -2,12 +2,30 @@
 # ____________   IMPORTS ________________
 # used to create a custom window for age calculator
 import tkinter as tk
+from tkinter import messagebox
 from tkinter import ttk
+
 # ____________   MAIN  ________________
 # Creating a custom window
 window = tk.Tk()
-window.geometry("500x230")
-window.config(bg="#F7DC6F")
+window.geometry("700x500")
+window.config(bg="#a7f2cd")
 window.resizable(width=False, height=False)
 window.title('Compare Prices Now!')
 
+# Labels for Heading and Subheadng of GUI
+lb_heading = tk.Label(window,
+                      text="Price Comparison Tool!",
+                      font=("Comic Sans", 20),
+                      fg="black",
+                      bg="#F7DC6F")
+lb_subheading = tk.Label(
+  window,
+  font=("Arial", 12),
+  text="Enter a store, a product, and a budget.",
+  fg="black",
+  bg="#F7DC6F")
+
+# Placing the elements on the screen
+lb_heading.place(x=200, y=5)
+lb_subheading.place(x=250, y=40)
